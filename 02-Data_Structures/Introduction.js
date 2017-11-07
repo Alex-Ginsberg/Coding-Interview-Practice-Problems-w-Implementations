5163120cb78548c1de784e5b3fb8de146b53f618
Alex Ginsberg <alex.ginsberg@richmond.edu>
2017-11-07 15:42:36 -0500

    Added introduction

---
+++ 02-Data_Structures/Introduction.js
@@ -0,0 +1,22 @@
+// Data Structures
+// ----------------------------------------------------------------------------
+
+// Contiguous Arrays
+    // Adjacent addresses in memory
+    // Fixed size and each element is the same size
+// Stacks
+    // Use array for storage
+    // LIFO (Last In First Out)
+    // Operations: push, pop
+// Queues
+    // Use array for storage
+    // FIFO (First In First Out)
+    // Operations: push, shift
+// Hash Tables
+    // Uses a hash function to convert to an index in a contiguous array
+    // Deal with collisions with:
+        // Open addressing: find the next empty bucket
+        // Separate chaining: every bucket stores a secondary data structure
+// Set
+    // Collection of unique elements
+    // None of the elements can be the set itself
