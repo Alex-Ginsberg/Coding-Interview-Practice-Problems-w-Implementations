68eb91ddbec5cc8e541624aa41825ef1d520ab03
Alex Ginsberg <alex.ginsberg@richmond.edu>
2017-11-08 14:35:46 -0500

    Added intro

---
+++ 03-Optimizations/Introduction.js
@@ -0,0 +1,14 @@
+// Optimizations
+// ----------------------------------------------------------------------------
+// Design algorithms that go faster, take up less space, reduce the amount of resources used
+// Understand that there is a trade-off between readability and optimal code 
+// Optimized code usually centers around chosing the best data structure for the situation
+// For review, here is a list of commonly used data structures:
+    // Tree (tries, binary search)
+    // Graphs
+    // Queue
+    // Stack
+    // Hash tables
+    // Linked lists
+// Sometimes sorting can help us search quickly
+// Dynamic programming: breaks down a big problem into sub-problems that are overlapping
